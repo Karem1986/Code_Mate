c1867b8873c1e91a0e21e7a2775e5adcc9f700b5
Karem1986 <karem.ortiz@outlook.com>
2020-09-14 15:41:57 +0200

    created book session page-not finished yet

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Homepage from "./pages/Homepage";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Switch, Route } from "react-router-dom"; //gives a website for each web page
 import "./App.css";
+import BookSession from "./pages/BookSession";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <NavBar />
         <Switch>
           <Route path="/" exact component={Homepage}></Route>
-          {/* <Route path="/code_session" component={Code_Session}></Route> */}
+          <Route path="/code_session" component={BookSession}></Route>
         </Switch>
       </div>
     </Router>
---
+++ src/pages/BookSession.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function BookSession() {
+  return (
+    <div>
+      <h2 className="h2-book-session">Book your session here</h2>
+      <div className="week">
+        <div className="day"></div>
+      </div>
+    </div>
+  );
+}
